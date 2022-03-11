49a3e2f437eaac3253df02bf203fb16bc545a243
Charlouze <me@charlouze.com>
2022-03-11 23:59:59 +0100

    @angular/fire init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'faircount',
+    appId: '1:981038919493:web:c958b38e406a581c61982c',
+    storageBucket: 'faircount.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDZ2_m-i-P-yubYg5Cwty4plyQ_LXL4ngo',
+    authDomain: 'faircount.firebaseapp.com',
+    messagingSenderId: '981038919493',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'faircount',
+    appId: '1:981038919493:web:c958b38e406a581c61982c',
+    storageBucket: 'faircount.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyDZ2_m-i-P-yubYg5Cwty4plyQ_LXL4ngo',
+    authDomain: 'faircount.firebaseapp.com',
+    messagingSenderId: '981038919493',
+  },
   production: false
 };
 
